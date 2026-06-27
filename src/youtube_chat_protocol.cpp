807bb8db35d6b73b7d1a0688fd6435ba08fb38b8
Cole Blakley <cblakley15@gmail.com>
2026-06-26 23:21:30 -0700

    Moved youtube::Protocol into own file
    Makes it easier to navigate codebase and isolates GPlugin-specific code into its own module

---
+++ src/youtube_chat_protocol.cpp
@@ -0,0 +1,206 @@
+/*
+purple-youtube
+Copyright (C) 2026 Cole Blakley
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#include "youtube_chat_protocol.hpp"
+#include <peel/Gio/Cancellable.h>
+#include <peel/Gio/Task.h>
+#include <peel/Purple/Account.h>
+#include <peel/Purple/AccountSettingString.h>
+#include <peel/Purple/ConversationManager.h>
+#include <peel/Purple/ConversationType.h>
+#include <peel/Purple/Core.h>
+#include <peel/Purple/Message.h>
+#include "youtube_chat_connection.hpp"
+
+namespace youtube {
+
+PEEL_CLASS_IMPL_DYNAMIC(Protocol, "YoutubeProtocol", purple::Protocol)
+
+peel::RefPtr<Protocol> Protocol::create()
+{
+    auto protocol = Object::create<Protocol>(
+        prop_id(), "prpl-youtube-chat",
+        prop_name(), "YouTube Live Chat",
+        prop_description(), "YouTube live chat support"
+        //"icon-name", "im-youtube",
+        //"icon-resource-path", "TODO"
+    );
+    return protocol;
+}
+
+peel::RefPtr<purple::AccountSettings> Protocol::vfunc_get_default_account_settings()
+{
+    auto account_settings = purple::AccountSettings::create();
+
+    // TODO: should these be in CredentialManager??
+    auto access_token = purple::AccountSettingString::create("access_token", "OAuth Access Token", "");
+    access_token->set_advanced(true);
+    account_settings->add_setting(std::move(access_token));
+
+    auto refresh_token = purple::AccountSettingString::create("refresh_token", "OAuth Refresh Token", "");
+    refresh_token->set_advanced(true);
+    account_settings->add_setting(std::move(refresh_token));
+
+    auto access_token_expiration = purple::AccountSettingString::create(
+        "access_token_expiration", "OAuth Access Token Expiration", "");
+    access_token_expiration->set_advanced(true);
+    account_settings->add_setting(std::move(access_token_expiration));
+
+    return account_settings;
+}
+
+peel::RefPtr<purple::Connection> Protocol::vfunc_create_connection(
+    purple::Account* account, peel::UniquePtr<glib::Error>*)
+{
+    return youtube::Connection::create(account);
+}
+
+peel::RefPtr<purple::ChannelJoinDetails> Protocol::vfunc_get_channel_join_details(purple::Account*)
+{
+    return purple::ChannelJoinDetails::create(
+        /*name_max_length=*/0,
+        /*nickname_supported=*/false, /*nickname_max_length=*/0,
+        /*password_supported=*/false, /*password_max_length=*/0
+    );
+}
+
+Task<peel::RefPtr<purple::Conversation>> Protocol::vfunc_join_channel_async(
+    purple::Account* account, purple::ChannelJoinDetails* channel_details, gio::Cancellable* cancellable)
+{
+    using ConvType = purple::ConversationType;
+
+    auto* connection = static_cast<youtube::Connection*>(account->get_connection());
+    auto* stream_url = channel_details->get_name();
+    auto error = co_await connection->join_live_chat_async(stream_url, cancellable);
+    if(error) {
+        co_return std::unexpected(std::move(error));
+    }
+    auto* conversation_manager = purple::Core::get_default()->get_conversation_manager();
+    auto channel_id = connection->get_channel_id();
+    if(auto* conversation = conversation_manager->find(account, ConvType::CHANNEL, channel_id)) {
+        conversation->set_title(connection->get_title());
+        co_return conversation;
+    }
+
+    auto conversation = purple::Conversation::create(account, ConvType::CHANNEL, channel_id);
+    conversation->set_online(true);
+    conversation->set_title(connection->get_title());
+    conversation_manager->add(conversation);
+    co_return conversation;
+}
+
+Task<void> Protocol::vfunc_leave_conversation_async(purple::Conversation* conversation, gio::Cancellable*)
+{
+    // TODO: might want to have client distinguish between a connection and a connected conversation
+    auto* connection = static_cast<youtube::Connection*>(conversation->get_connection());
+    peel::UniquePtr<glib::Error> error;
+    connection->vfunc_disconnect("Left conversation", &error);
+    co_return error;
+}
+
+Task<void> Protocol::vfunc_send_message_async(
+    purple::Conversation* conversation, purple::Message* message, gio::Cancellable* cancellable)
+{
+    auto* connection = static_cast<youtube::Connection*>(conversation->get_connection());
+    // TODO: eventually need to stringify the message contents better
+    auto error = co_await connection->send_message_async(message->get_contents(), cancellable);
+    co_return error;
+}
+
+void Protocol::Class::init()
+{
+    override_vfunc_create_connection<Protocol>();
+    override_vfunc_get_default_account_settings<Protocol>();
+}
+
+void Protocol::init_type(gobject::TypeModule* type_module, peel::Type type)
+{
+    PEEL_IMPLEMENT_INTERFACE_DYNAMIC(type_module, type, purple::ProtocolConversation);
+}
+
+void Protocol::init_interface(purple::ProtocolConversation::Iface* iface)
+{
+    iface->override_vfunc_get_channel_join_details<Protocol>();
+    // TODO: unsupported currently in peel. Overriding manually for now
+    auto* iface_class = reinterpret_cast<PurpleProtocolConversationInterface*>(iface);
+    iface_class->join_channel_async =
+        [](PurpleProtocolConversation* protocol, PurpleAccount* account, PurpleChannelJoinDetails* details,
+            GCancellable* cancellable, GAsyncReadyCallback callback, gpointer data) {
+            auto* self = reinterpret_cast<youtube::Protocol*>(protocol);
+            auto* task = reinterpret_cast<gio::Task*>(g_task_new(protocol, cancellable, callback, data));
+            auto* _peel_account = reinterpret_cast<purple::Account*>(account);
+            auto* _peel_details = reinterpret_cast<purple::ChannelJoinDetails*>(details);
+            [](youtube::Protocol* self, purple::Account* account, purple::ChannelJoinDetails* details,
+                gio::Task* task) -> VoidTask {
+                auto conversation = co_await self->vfunc_join_channel_async(
+                    account, details, task->get_cancellable());
+                if(conversation.has_value()) {
+                    task->return_pointer(std::move(conversation.value()).release_ref(), g_object_unref);
+                } else {
+                    task->return_error(conversation.error()->copy());
+                }
+            }(self, _peel_account, _peel_details, task).start();
+        };
+    iface_class->join_channel_finish =
+        [](PurpleProtocolConversation*, GAsyncResult* result, GError** error) {
+            return (PurpleConversation*)g_task_propagate_pointer(G_TASK(result), error);
+        };
+    iface_class->leave_conversation_async =
+        [](PurpleProtocolConversation* protocol, PurpleConversation* conversation, GCancellable* cancellable,
+            GAsyncReadyCallback callback, gpointer data) {
+            auto* self = reinterpret_cast<youtube::Protocol*>(protocol);
+            auto* task = reinterpret_cast<gio::Task*>(g_task_new(protocol, cancellable, callback, data));
+            auto* _peel_conversation = reinterpret_cast<purple::Conversation*>(conversation);
+            [](youtube::Protocol* self, purple::Conversation* conversation, gio::Task* task) -> VoidTask {
+                auto error = co_await self->vfunc_leave_conversation_async(
+                    conversation, task->get_cancellable());
+                if(error) {
+                    task->return_error(error->copy());
+                } else {
+                    task->return_boolean(true);
+                }
+            }(self, _peel_conversation, task).start();
+        };
+    iface_class->leave_conversation_finish = [](PurpleProtocolConversation*, GAsyncResult* result,
+                                                GError** error) {
+        return g_task_propagate_boolean(G_TASK(result), error);
+    };
+    iface_class->send_message_async =
+        [](PurpleProtocolConversation* protocol, PurpleConversation* conversation, PurpleMessage* message,
+            GCancellable* cancellable, GAsyncReadyCallback callback, gpointer data) {
+            auto* self = reinterpret_cast<youtube::Protocol*>(protocol);
+            auto* task = reinterpret_cast<gio::Task*>(g_task_new(protocol, cancellable, callback, data));
+            auto* _peel_conversation = reinterpret_cast<purple::Conversation*>(conversation);
+            auto* _peel_message = reinterpret_cast<purple::Message*>(message);
+            [](youtube::Protocol* self, purple::Conversation* conversation, purple::Message* message,
+                gio::Task* task) -> VoidTask {
+                auto error = co_await self->vfunc_send_message_async(
+                    conversation, message, task->get_cancellable());
+                if(error) {
+                    task->return_error(error->copy());
+                } else {
+                    task->return_boolean(true);
+                }
+            }(self, _peel_conversation, _peel_message, task).start();
+        };
+    iface_class->send_message_finish =
+        [](PurpleProtocolConversation*, GAsyncResult* result, GError** error) {
+            return g_task_propagate_boolean(G_TASK(result), error);
+        };
+}
+
+} // namespace youtube
---
+++ src/youtube_chat_protocol.hpp
@@ -0,0 +1,61 @@
+/*
+purple-youtube
+Copyright (C) 2026 Cole Blakley
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#include <peel/class.h>
+#include <peel/GLib/Error.h>
+#include <peel/Purple/AccountSettings.h>
+#include <peel/Purple/ChannelJoinDetails.h>
+#include <peel/Purple/Conversation.h>
+#include <peel/Purple/Connection.h>
+#include <peel/Purple/Protocol.h>
+#include <peel/Purple/ProtocolConversation.h>
+#include <peel/UniquePtr.h>
+#include "youtube_types.hpp"
+#include "task.hpp"
+
+namespace peel::Purple {
+class Account;
+class Message;
+}
+
+namespace peel::Gio {
+class Cancellable;
+}
+
+namespace youtube {
+
+class Protocol final : public purple::Protocol {
+    PEEL_SIMPLE_DYNAMIC_CLASS(Protocol, purple::Protocol)
+public:
+    static void init_type(gobject::TypeModule*, peel::Type);
+    static void init_interface(purple::ProtocolConversation::Iface*);
+    void init(Class*) {}
+
+    static peel::RefPtr<Protocol> create();
+
+    peel::RefPtr<purple::AccountSettings> vfunc_get_default_account_settings();
+    peel::RefPtr<purple::Connection> vfunc_create_connection(purple::Account*, peel::UniquePtr<glib::Error>*);
+    peel::RefPtr<purple::ChannelJoinDetails> vfunc_get_channel_join_details(purple::Account*);
+    Task<peel::RefPtr<purple::Conversation>> vfunc_join_channel_async(
+        purple::Account*, purple::ChannelJoinDetails*, gio::Cancellable*);
+    Task<void> vfunc_leave_conversation_async(purple::Conversation*, gio::Cancellable*);
+    Task<void> vfunc_send_message_async(purple::Conversation*, purple::Message*, gio::Cancellable*);
+};
+
+} // namespace youtube
